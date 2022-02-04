c60b556dd87610814f4be398a2062e938c7b0107
seungheon <seungheon@apexsoft.co.kr>
2022-02-04 14:39:37 +0900

    초기 세팅

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes : [
+        {
+            path : '/',
+            component : ''
+        },
+        {
+            path : '',
+            component : ''
+        },
+        {
+            path : '',
+            component : ''
+        }
+    ]
+})
---
+++ src/views/layout/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-lg">g하하</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
